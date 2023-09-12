2783e22ace4c0a65e0d1967942a9b1de7060b4e4
siam13220 <sh24072407@gmail.com>
2023-09-12 15:07:38 +0600

    Explore state,event,use effect

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+import Counter from './Counter';
+import Friends from './Friends';
+import Team from './Team';
+import Users from './Users';
+
+function App() {
+
+  function handleClick() {
+    alert('click me');
+  }
+  const handleClick2 = () => {
+    alert('button 2 clicked');
+  }
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      <Friends></Friends>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={() => { alert('third clicked') }}>third</button>
+      <button onClick={() => addToFive(3)}>fourth</button>
+    </>
+  )
+}
+
+export default App
